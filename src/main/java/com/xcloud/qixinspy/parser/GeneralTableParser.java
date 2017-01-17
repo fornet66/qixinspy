d8b09e10c9c5ac66a0f24dd26b44b9fb30cc8dfe
xienan <xienan@yunjiacloud.com>
2017-01-18 01:05:26 +0800

    add table parser

--- src/main/java/com/xcloud/qixinspy/parser/GeneralTableParser.java
+++ src/main/java/com/xcloud/qixinspy/parser/GeneralTableParser.java
@@ -3,15 +3,24 @@ package com.xcloud.qixinspy.parser;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class GeneralTableParser {
 
     public List<List<String>> parse(WebElement element) {
-        List<List<String>> tbodylist = new ArrayList<List<String>>();
-        List<String> trlist = new ArrayList<String>();
-        tbodylist.add(trlist);
-        return tbodylist;
+        List<List<String>> rows = new ArrayList<List<String>>();
+        List<WebElement> trs = element.findElements(By.tagName("tr"));
+        for (WebElement tr : trs) {
+            List<String> values = new ArrayList<String>();
+            List<WebElement> tds = tr.findElements(By.tagName("td"));
+            for (WebElement td : tds) {
+                String value = td.getText();
+                values.add(value);
+            }
+            rows.add(values);
+        }
+        return rows;
     }
 
 }
